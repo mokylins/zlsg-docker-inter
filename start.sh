c79f680f829df3d0bfc87411a128499488805b3d
刘雷 <liulei@mokylin.com>
2020-05-11 16:41:06 +0800

    更新最新版本-test

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@
 # -H{/*/}		程序工作目录
 
 #引入JAVA启动选项
-. ./java_option.sh
+#. ./java_option.sh
 
 
 #*********************************
@@ -15,8 +15,15 @@
 #*********************************
 run()
 {
-	nohup java $JAVA_OPTS $APP_OPTS -jar ${1}.jar >> "$(basename ${1}).log" 2>&1 &
+	#nohup java $JAVA_OPTS -jar ${1}.jar >> "$(basename ${1}).log" 2>&1 &
+	#
+	nohup java -jar ${1}.jar | tee "$(basename ${1}).log" &
+	#直接覆盖日志文件   
+	# | tee ./t.log   
+	#将输出内容附加到日志文件   
+	# | tee -a ./t.log
 }
+
 
 ##########
 run "$(pwd)/dip"

7fbbe7dc4dfa5f8e038b7669dd6365e8d96375e6
刘雷 <liulei@mokylin.com>
2020-05-12 11:35:17 +0800

    修改为简单启动

--- start.sh
+++ start.sh
@@ -15,14 +15,15 @@
 #*********************************
 run()
 {
+	java -jar ${1}.jar
 	#nohup java $JAVA_OPTS -jar ${1}.jar >> "$(basename ${1}).log" 2>&1 &
 	#
-	nohup java -jar ${1}.jar | tee "$(basename ${1}).log" &
+	#nohup java -jar ${1}.jar | tee "$(basename ${1}).log" 2>&1 &
 	#直接覆盖日志文件   
 	# | tee ./t.log   
 	#将输出内容附加到日志文件   
 	# | tee -a ./t.log
-	echo $! > "$(basename ${1}).pid"
+	#echo $! > "$(basename ${1}).pid"
 	#关闭
 	#PID=$(cat $(basename ${1}).pid)
 	#kill -9 $PID
